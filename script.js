35476b02fe9373153f6ce592ea7e053dc318159c
gabrielbdsm <gabrielmartiliano2@gmail.com>
2021-10-04 06:55:28 -0300

    area de conhecimento �

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.querySelector(".hambu").addEventListener("click",() =>
-document.querySelector(".container").classList.toggle("show-menu")
+document.querySelector(".hambu").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
 );
